f367322bde8ca0eb80334f601d8dcc24969636d2
Raihan-Ardhanis <raihan_raharja_33rpl@student.smktelkom-mlg.sch.id>
2024-08-26 14:05:08 +0700

    first commit

---
+++ Guru.java
@@ -0,0 +1,30 @@
+public class Guru {
+    int nip;
+    String nama, mapel, status;
+
+
+    public Guru(int i, String n, String m, String s) {
+        nip = i;
+        nama = n;
+        mapel = m;
+        status = s;
+
+    }
+
+    public void print() {
+            System.out.println("NIP : "+ nip);
+            System.out.println("NAMA : " +nama);
+            System.out.println("MAPEL : " +mapel);
+            System.out.println("STATUS : " +status);
+
+       
+
+
+
+
+
+    }
+    
+
+    
+}
---
+++ TesGuru.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class TesGuru {
+    //1.NIP
+    //2.NAMA
+    //3.MAPEL
+    //4.STATUS
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nama : ");
+        String nama = input.nextLine();
+        System.out.println("Mapel :");
+        String mapel = input.nextLine();
+        System.out.println("Status :");
+        String status = input.nextLine();
+        System.out.println("NIP :");
+        int nip = input.nextInt();
+
+        input.close();
+
+        Guru BuPasha = new Guru(nip, nama, mapel,status);
+        BuPasha.print();
+
+        // Guru pakAbidin =  new Guru(1111, "Pak Abidin", "Matematika", "Tetap");
+
+        // Guru pakMunif =  new Guru(1122, "Pak Munif", "Agama", "Part Time");
+
+        // Guru buPasha =  new Guru(1133, "Bu Pasha", "Produktif RPL", "Tetap");
+
+        // Guru pakFalah =  new Guru(1144, "Pak Falah", "Seni Budaya", "Full Time");
+        
+        // Guru buAsry =  new Guru(1155, "Bu Asry", "Bahasa Indonesia", "TETAP");
+
+
+
+    }
+
+    
+
+
+        
+    
+    
+}
